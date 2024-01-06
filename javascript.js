bd15329df1c39c7c3ca6ec6c4bd3ac839791e84f
AntoineRiouxDemers <152810044+AntoineRiouxDemers@users.noreply.github.com>
2024-01-06 02:15:03 +0000

    Added calculator inputs read
    Now read calculator inputs
    
    Each inputs are separated, operators, clear and numbers

--- javascript.js
+++ javascript.js
@@ -1,12 +1,31 @@
 const inputs = document.querySelectorAll('.inputs');
 const nine = document.getElementById('nine');
+const answer = document.getElementById('showMath');
+let currentNumber = 0;
+let currentAnswer = 0;
 
 inputs.forEach(input => input.addEventListener('click', () => readInput(input)));
 
 function readInput(input) {
-  switch (input.id) {
-    case 'nine':
-      console.log('nine');
-      break;
-  }
+  (input.classList.contains('numbers')) ? isANumber(input) :
+    (input.classList.contains('operators')) ? isAnOperator(input) : clearCalculator(); 
+}
+
+function isANumber(input){
+  console.log(input.id);
+  (currentNumber == 0) ? currentNumber = input.textContent : currentNumber += input.textContent;
+  console.log(currentNumber);
+  answer.textContent = currentNumber;
+}
+
+function isAnOperator(input){
+    
+}
+
+function clearCalculator(){
+  console.log('clear');
+  currentNumber = 0;
+  currentAnswer = 0;
+  answer.textContent = currentNumber;
+  
 }
